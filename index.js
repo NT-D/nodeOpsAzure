f13b44b11af0e01f4ffee3d6da3bb17a97ca34fc
Masayuki Ota <masota@microsoft.com>
2017-11-29 12:45:58 +0900

    Add simple web app with restify

---
+++ index.js
@@ -0,0 +1,19 @@
+var restify = require('restify');
+
+//Create Server
+var server = restify.createServer();
+
+//Set endpoint
+server.get('/api/test',log);
+server.get('/api/test2',log);
+
+//Set functoin
+function log(req,res,next){
+    console.log('req.url: '+req.url);
+    res.send("Success");
+}
+
+//Listen
+server.listen(8080,function(){
+    console.log('%s listening at %s', server.name, server.url);
+});
